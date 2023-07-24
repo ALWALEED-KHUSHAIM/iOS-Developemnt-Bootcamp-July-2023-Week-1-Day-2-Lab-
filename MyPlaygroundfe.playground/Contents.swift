3adec3cb7dfddeaf753be9624a05bdb6579c609e
ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>
2023-07-25 01:31:00 +0300

    lap 2

---
+++ MyPlaygroundfe.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+//----------------1---------------
+var score=60
+score+=10
+print(score)
+
+
+//_________________2________________
+
+if score > 9
+{print("Pass")}
+else
+{print("Fail")}
+//-----------------3-----------------
+
+let names = ["waleed", "ahmad", "khalid"]
+
+for name in names {
+    print(name)
+}
+//_________________4_________________
+
+let fod =  [1:"appil",3:"tometo"]
+for name in fod {
+   print(name)
+}
+
+//_________________5__________________
+
+let myname : String = "waleed"
+
+print("welcome to " + myname)
+
+
+
